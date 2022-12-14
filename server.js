28909df5d6fc777ec774080f118b097a242e1465
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2022-12-14 21:27:06 +0100

    Fixed typo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const http = require("http");
-const ws = require("wss");
+const ws = require("ws");
 
 const wss = new ws.Server({ noServer: true });
 const clients = new Set();
